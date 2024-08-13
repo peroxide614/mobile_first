c7e33bbb6f9c3f8ae1aee54eda1e291d9fe88db2
DESKTOP-JGJSHGL\bih <peroxide614@gmail.com>
2024-08-13 18:44:40 +0300

    hotel-info block done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import apartment from './image/apart_img.png';
+import star from './image/star.png';
 
 function App() {
   return (
@@ -10,8 +11,15 @@ function App() {
           {/* <div className="int-ext"> Interior | Exterior</div> */}
         </img>
       </div>
-      <div className="hotel-info">
-        <div className="hotel-name"></div>
+      <div className="hotel">
+        <div className="hotel-info">
+          <div className="hotel-name">Raddison Hotel</div>
+          <div className="hotel-adress">Thane West</div>
+          <div className="hotel-rating-wrapper">
+            <img src={star} alt="Star" className="star-icon" />
+            <div className="hotel-rating">4.3/5</div>
+          </div>
+        </div>
         <div className="hotel-price"></div>
         <div className="hotel-options"></div>
       </div>
