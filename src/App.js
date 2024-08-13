1e22803aef3162d138284a94d1ce24ecea330e1e
DESKTOP-JGJSHGL\bih <peroxide614@gmail.com>
2024-08-13 18:08:55 +0300

    skipped icon, created structure

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ function App() {
   return (
     <div className="full-page">
       <div className="hotel-img">
-        <img src={apartment}></img>
+        <img src={apartment}>
+          {/* <div className="int-ext"> Interior | Exterior</div> */}
+        </img>
+      </div>
+      <div className="hotel-info">
+        <div className="hotel-name"></div>
+        <div className="hotel-price"></div>
+        <div className="hotel-options"></div>
       </div>
     </div>
   );
