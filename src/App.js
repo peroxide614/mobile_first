8fb45fdfeb4072bcad3f90c55c8065703eb28373
DESKTOP-JGJSHGL\bih <peroxide614@gmail.com>
2024-08-13 18:01:34 +0300

    added apartment img, deleted unnecessary img

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import './App.css';
+import apartment from './image/apart_img.png';
 
 function App() {
-  return <div>adfsd</div>;
+  return (
+    <div className="full-page">
+      <div className="hotel-img">
+        <img src={apartment}></img>
+      </div>
+    </div>
+  );
 }
 export default App;
